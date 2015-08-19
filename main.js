2a5edfc28c93c042e6ed7aacd13408af2ad9d5da
= <=>
2015-08-19 12:39:43 -0700

    replace adding eventlistners to all images with event.target

--- main.js
+++ main.js
@@ -2,55 +2,43 @@ var choices = ['rock', 'paper', 'scissors'];
 var humanChoice;
 var compChoice;
 var score = {wins: 0, loses: 0};
-var images = document.querySelectorAll("img");
 var scoreDisplay = document.getElementById("score");
 var whoChoseWhat = document.createElement("p");
 var wins = document.createElement("p");
 var loses = document.createElement("p");
 var body = document.querySelector("body");
 var button = document.querySelector("button");
+var images = document.querySelector(".images");
 
-for (var i = 0; i < images.length; i++) {
-  images[i].addEventListener("click", function(){
-    humanChoice = this.getAttribute("id");
-      compChoice = choices[Math.floor(Math.random() * choices.length)];
-       if (compChoice === humanChoice) {
-         score.loses += 0;
-         score.wins += 0;
-       } else if (compChoice === "rock" && humanChoice === "scissors") {
-        score.loses += 1;
-      } else if (compChoice === "paper" && humanChoice === "rock") {
-        score.loses += 1;
-      } else if (compChoice === "scissors" && humanChoice === "paper") {
-        score.loses += 1;
-      } else {
-        score.wins += 1;
-      }
+images.addEventListener("click", function(e){
+  humanChoice = e.target.id;
+    compChoice = choices[Math.floor(Math.random() * choices.length)];
+     if (compChoice === humanChoice) {
+      // do nothing
+     } else if (compChoice === "rock" && humanChoice === "scissors") {
+       score.loses += 1;
+     } else if (compChoice === "paper" && humanChoice === "rock") {
+       score.loses += 1;
+     } else if (compChoice === "scissors" && humanChoice === "paper") {
+       score.loses += 1;
+     } else {
+       score.wins += 1;
+     }
 
-      whoChoseWhat.innerHTML = "";
-      whoChoseWhat.innerHTML = "You chose " + humanChoice + "//" + "Opponent chose " + compChoice;
-      whoChoseWhat.setAttribute("id", "whoChoseWhat");
-      body.appendChild(whoChoseWhat);
-      scoreDisplay.innerHTML = "";
-      wins.innerHTML = "Wins: " + score.wins;
-      loses.innerHTML = "Loses: " + score.loses;
-      scoreDisplay.appendChild(wins);
-      scoreDisplay.appendChild(loses);
+    whoChoseWhat.innerHTML = "You chose " + humanChoice + "//" + "Opponent chose " + compChoice;
+    whoChoseWhat.setAttribute("id", "whoChoseWhat");
+    body.appendChild(whoChoseWhat);
+    wins.innerHTML = "Wins: " + score.wins;
+    loses.innerHTML = "Loses: " + score.loses;
+    scoreDisplay.appendChild(wins);
+    scoreDisplay.appendChild(loses);
+});
 
-  });
-}
 
-button.addEventListener("click", function(e){
-  e.preventDefault();
+button.addEventListener("click", function(){
   scoreDisplay.innerHTML = "";
   score.wins = 0;
   score.loses = 0;
 });
 
 body.appendChild(button);
-
-
-
-
-
-  console.log("You chose " + humanChoice + "\t//\t" + "Your opponent chose " + compChoice);
